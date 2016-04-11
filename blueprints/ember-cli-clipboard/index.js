911e4e6c9f24965e27771baa2102ffebdfd5dcc6
Robert Jackson <robert.w.jackson@me.com>
2016-04-10 22:18:29 -0400

    Remove bower package requirement.
    This replaces the blueprint adding `clipboard.js` to the consuming
    applications bower dependencies, with a direct NPM dependency.
    
    This will help as ember-cli continues its migration away from bower (and
    eventually removes bower support from the core into an optional addon).

--- blueprints/ember-cli-clipboard/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  description: 'install ember-cli-clipboard into a typical project',
-
-  normalizeEntityName: function() {},
-
-  afterInstall: function (options) {
-    return this.addBowerPackageToProject('clipboard', '~1.5.5');
-  }
-};
