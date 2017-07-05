aeafe61b03de87617fba8b74dd468d52498f9741
Sivakumar Kailasam <sivakumar-kailasam@users.noreply.github.com>
2017-07-05 08:24:33 +0530

    Add the additional window check for fastboot

--- addon/helpers/is-clipboard-supported.js
+++ addon/helpers/is-clipboard-supported.js
@@ -1,4 +1,4 @@
 import Ember from 'ember';
 
-export const isClipboardSupported = window.Clipboard ? window.Clipboard.isSupported: () => false;
+export const isClipboardSupported = window && window.Clipboard ? window.Clipboard.isSupported : () => false;
 export default Ember.Helper.helper(isClipboardSupported);
