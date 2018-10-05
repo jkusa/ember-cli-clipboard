c1ee795daf8a05e87a86f389c92b59d148beccb9
Jon Kilroy <kilroy@oath.com>
2018-10-05 15:30:10 -0500

    Use ember-auto-import for dummy app

--- tests/dummy/app/components/code-block.js
+++ tests/dummy/app/components/code-block.js
@@ -1,5 +1,10 @@
 import Component from '@ember/component';
 import layout from '../templates/components/code-block';
+import hljs from 'highlight.js/lib/highlight';
+import handlebars from 'highlight.js/lib/languages/handlebars';
+
+hljs.registerLanguage('handlebars', handlebars);
+
 
 export default Component.extend({
   layout,
