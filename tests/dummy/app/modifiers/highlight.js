cbadf3f2a7e348bb7cb4bd50a63197b2375c41e9
jkusa <jkilroy@gmail.com>
2020-09-19 13:11:23 -0500

    Update uuid and highlight.js

--- tests/dummy/app/modifiers/highlight.js
+++ tests/dummy/app/modifiers/highlight.js
@@ -1,5 +1,5 @@
 import { modifier } from 'ember-modifier';
-import hljs from 'highlight.js/lib/highlight';
+import hljs from 'highlight.js/lib/core';
 import handlebars from 'highlight.js/lib/languages/handlebars';
 
 hljs.registerLanguage('handlebars', handlebars);
