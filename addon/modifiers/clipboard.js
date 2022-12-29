49e0a9fb9c7dc3126a23976aba9a74da8ef783d0
oliverlj <oliverlj@protonmail.com>
2022-12-29 11:48:54 +0100

    refactor: ember-modifier 4 depreciations

--- addon/modifiers/clipboard.js
+++ addon/modifiers/clipboard.js
@@ -6,54 +6,57 @@ import { guidFor } from '@ember/object/internals';
 
 const CLIPBOARD_EVENTS = ['success', 'error'];
 
-export default modifier(function clipboard(element, params, hash) {
-  const {
-    action = 'copy',
-    container,
-    /*
-     * delegateClickEvent true - scope event listener to this element
-     * delegateClickEvent false - scope event listener to document.body (ClipboardJS)
-     */
-    delegateClickEvent = true,
-    target,
-    text,
-  } = hash;
+export default modifier(
+  function clipboard(element, params, hash) {
+    const {
+      action = 'copy',
+      container,
+      /*
+       * delegateClickEvent true - scope event listener to this element
+       * delegateClickEvent false - scope event listener to document.body (ClipboardJS)
+       */
+      delegateClickEvent = true,
+      target,
+      text,
+    } = hash;
 
-  element.setAttribute('data-clipboard-action', action);
+    element.setAttribute('data-clipboard-action', action);
 
-  if (!isBlank(text)) {
-    element.setAttribute('data-clipboard-text', text);
-  }
-  if (!isBlank(target)) {
-    element.setAttribute('data-clipboard-target', target);
-  }
+    if (!isBlank(text)) {
+      element.setAttribute('data-clipboard-text', text);
+    }
+    if (!isBlank(target)) {
+      element.setAttribute('data-clipboard-target', target);
+    }
 
-  if (isBlank(element.dataset.clipboardId)) {
-    element.setAttribute('data-clipboard-id', guidFor(element));
-  }
+    if (isBlank(element.dataset.clipboardId)) {
+      element.setAttribute('data-clipboard-id', guidFor(element));
+    }
 
-  const trigger =
-    delegateClickEvent === false
-      ? element
-      : `[data-clipboard-id=${element.dataset.clipboardId}]`;
+    const trigger =
+      delegateClickEvent === false
+        ? element
+        : `[data-clipboard-id=${element.dataset.clipboardId}]`;
 
-  const clipboard = new ClipboardJS(trigger, {
-    text: typeof text === 'function' ? text : undefined,
-    container:
-      typeof container === 'string'
-        ? document.querySelector(container)
-        : container,
-    target,
-  });
-
-  CLIPBOARD_EVENTS.forEach((event) => {
-    clipboard.on(event, () => {
-      if (!element.disabled) {
-        const action = hash[`on${capitalize(event)}`];
-        action?.(...arguments);
-      }
+    const clipboard = new ClipboardJS(trigger, {
+      text: typeof text === 'function' ? text : undefined,
+      container:
+        typeof container === 'string'
+          ? document.querySelector(container)
+          : container,
+      target,
     });
-  });
 
-  return () => clipboard.destroy();
-});
+    CLIPBOARD_EVENTS.forEach((event) => {
+      clipboard.on(event, () => {
+        if (!element.disabled) {
+          const action = hash[`on${capitalize(event)}`];
+          action?.(...arguments);
+        }
+      });
+    });
+
+    return () => clipboard.destroy();
+  },
+  { eager: false }
+);
