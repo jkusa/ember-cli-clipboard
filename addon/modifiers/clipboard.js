0a7ef57de0c021161e27c9bdf46243d38c3ad09e
Charles Fries <charliefries@icloud.com>
2023-08-16 18:17:17 -0700

    Support ember-modifier 3.x and 4.x (#396)

--- addon/modifiers/clipboard.js
+++ addon/modifiers/clipboard.js
@@ -3,60 +3,68 @@ import ClipboardJS from 'clipboard';
 import { isBlank } from '@ember/utils';
 import { capitalize } from '@ember/string';
 import { guidFor } from '@ember/object/internals';
+import { macroCondition, dependencySatisfies } from '@embroider/macros';
 
 const CLIPBOARD_EVENTS = ['success', 'error'];
 
-export default modifier(
-  function clipboard(element, params, hash) {
-    const {
-      action = 'copy',
-      container,
-      /*
-       * delegateClickEvent true - scope event listener to this element
-       * delegateClickEvent false - scope event listener to document.body (ClipboardJS)
-       */
-      delegateClickEvent = true,
-      target,
-      text,
-    } = hash;
+function clipboard(element, params, hash) {
+  const {
+    action = 'copy',
+    container,
+    /*
+     * delegateClickEvent true - scope event listener to this element
+     * delegateClickEvent false - scope event listener to document.body (ClipboardJS)
+     */
+    delegateClickEvent = true,
+    target,
+    text,
+  } = hash;
 
-    element.setAttribute('data-clipboard-action', action);
+  element.setAttribute('data-clipboard-action', action);
 
-    if (!isBlank(text)) {
-      element.setAttribute('data-clipboard-text', text);
-    }
-    if (!isBlank(target)) {
-      element.setAttribute('data-clipboard-target', target);
-    }
+  if (!isBlank(text)) {
+    element.setAttribute('data-clipboard-text', text);
+  }
+  if (!isBlank(target)) {
+    element.setAttribute('data-clipboard-target', target);
+  }
 
-    if (isBlank(element.dataset.clipboardId)) {
-      element.setAttribute('data-clipboard-id', guidFor(element));
-    }
+  if (isBlank(element.dataset.clipboardId)) {
+    element.setAttribute('data-clipboard-id', guidFor(element));
+  }
 
-    const trigger =
-      delegateClickEvent === false
-        ? element
-        : `[data-clipboard-id=${element.dataset.clipboardId}]`;
+  const trigger =
+    delegateClickEvent === false
+      ? element
+      : `[data-clipboard-id=${element.dataset.clipboardId}]`;
 
-    const clipboard = new ClipboardJS(trigger, {
-      text: typeof text === 'function' ? text : undefined,
-      container:
-        typeof container === 'string'
-          ? document.querySelector(container)
-          : container,
-      target,
+  const clipboard = new ClipboardJS(trigger, {
+    text: typeof text === 'function' ? text : undefined,
+    container:
+      typeof container === 'string'
+        ? document.querySelector(container)
+        : container,
+    target,
+  });
+
+  CLIPBOARD_EVENTS.forEach((event) => {
+    clipboard.on(event, () => {
+      if (!element.disabled) {
+        const action = hash[`on${capitalize(event)}`];
+        action?.(...arguments);
+      }
     });
+  });
 
-    CLIPBOARD_EVENTS.forEach((event) => {
-      clipboard.on(event, () => {
-        if (!element.disabled) {
-          const action = hash[`on${capitalize(event)}`];
-          action?.(...arguments);
-        }
-      });
-    });
+  return () => clipboard.destroy();
+}
 
-    return () => clipboard.destroy();
-  },
-  { eager: false }
-);
+let clipboardModifier;
+
+if (macroCondition(dependencySatisfies('ember-modifier', '4.x'))) {
+  clipboardModifier = modifier(clipboard, { eager: false });
+} else {
+  clipboardModifier = modifier(clipboard);
+}
+
+export default clipboardModifier;
