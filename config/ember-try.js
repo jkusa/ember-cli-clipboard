8247e79a641ed1ca04555343dcf5e134849ca9e9
jkusa <jkilroy@gmail.com>
2023-08-17 18:31:47 -0500

    test: fixing compatibility tests

--- config/ember-try.js
+++ config/ember-try.js
@@ -7,18 +7,38 @@ module.exports = async function () {
   return {
     scenarios: [
       {
-        name: 'ember-lts-3.24',
+        name: 'ember-lts-3.28',
         npm: {
           devDependencies: {
-            'ember-source': '~3.24.3',
+            'ember-resolver': '^8.0.0',
+            'ember-source': '~3.28.0',
+            '@ember/test-helpers': '^2.9.3',
+            'ember-qunit': '^6.2.0',
           },
         },
       },
       {
-        name: 'ember-lts-3.28',
+        name: 'ember-lts-4.4',
         npm: {
           devDependencies: {
-            'ember-source': '~3.28.0',
+            'ember-resolver': '^8.0.0',
+            'ember-source': '~4.4.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-4.8',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.8.0',
+          },
+        },
+      },
+      {
+        name: 'ember-lts-4.12',
+        npm: {
+          devDependencies: {
+            'ember-source': '~4.12.0',
           },
         },
       },
@@ -57,7 +77,10 @@ module.exports = async function () {
         },
         npm: {
           devDependencies: {
+            'ember-resolver': '^8.0.0',
             'ember-source': '~3.28.0',
+            '@ember/test-helpers': '^2.9.3',
+            'ember-qunit': '^6.2.0',
           },
           ember: {
             edition: 'classic',
