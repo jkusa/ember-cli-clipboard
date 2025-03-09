070bf883f62a253353095918a0796d5e0cea7eb6
Charles Fries <charliefries@icloud.com>
2025-03-09 12:40:29 -0700

    Add ClipboardJS types

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,17 @@
 declare module 'ember-cli-clipboard/components/copy-button' {
   import Component from '@glimmer/component';
+  import ClipboardJS from 'clipboard';
 
   export default class CopyButton extends Component<{
     Args: {
-      text: string;
-      target?: string;
-      action?: 'copy' | 'cut';
-      container?: string | Element;
+      text?: string | ClipboardJS.Options['text'];
+      target?: string | ClipboardJS.Options['target'];
+      action?: 'cut' | 'copy' | ClipboardJS.Options['action'];
+      container?: string | ClipboardJS.Options['container'];
       delegateClickEvent?: boolean;
-      buttonType?: string;
-      onSuccess?: () => void;
-      onError?: () => void;
+      buttonType?: HTMLButtonElement['type'];
+      onSuccess?: (event: ClipboardJS.Event) => void;
+      onError?: (event: ClipboardJS.Event) => void;
     };
     Blocks: {
       default: [];
